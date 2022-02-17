be904902f3f3e0a6127e4e1fe0752447c8b9d06b
lincolnnguyen18 <letsmakeaworld@icloud.com>
2022-02-17 11:10:40 -0600

    backup

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -7,6 +7,7 @@ const $login_form = document.querySelectorAll('#login-form')[0];
 const $register_form = document.querySelectorAll('#register-form')[0];
 
 let mode = 'login';
+let submitted = false;
 
 $register_link.addEventListener('click', () => {
   $login_form.classList.toggle('hidden');
@@ -25,7 +26,7 @@ const $register_button = document.querySelectorAll('#register-button')[0];
 const $login_error = document.querySelectorAll('#login-error')[0];
 const $register_error = document.querySelectorAll('#register-error')[0];
 
-const display_error = (text) => {
+const display_message = (text) => {
   error = null;
   if (mode == 'login') {
     error = $login_error;
@@ -40,6 +41,9 @@ const display_error = (text) => {
 };
 
 $login_button.addEventListener('click', () => {
+  if (submitted) {
+    return;
+  }
   let username = document.querySelectorAll('#login-username-input')[0].value;
   let password = document.querySelectorAll('#login-password-input')[0].value;
   let data = { username, password };
@@ -53,19 +57,23 @@ $login_button.addEventListener('click', () => {
   .then(res => res.json())
   .then(json => {
     if (!json.error) {
+      submitted = true;
       window.location.href = '/';
     } else {
-      display_error(json.error);
+      display_message(json.error);
     }
   });
 });
 
 $register_button.addEventListener('click', () => {
+  if (submitted) {
+    return;
+  }
   let username = document.querySelectorAll('#register-username-input')[0].value;
   let password = document.querySelectorAll('#register-password-input')[0].value;
   let confirm_password = document.querySelectorAll('#register-confirm-password-input')[0].value;
   if (password != confirm_password) {
-    display_error('Passwords do not match');
+    display_message('Passwords do not match');
     return;
   }
   let data = { username, password };
@@ -79,9 +87,23 @@ $register_button.addEventListener('click', () => {
   .then(res => res.json())
   .then(json => {
     if (!json.error) {
-      window.location.href = '/';
+      submitted = true;
+      $register_error.classList.remove('hidden');
+      let remaining_time = 3;
+      const interval_function = () => {
+        $register_error.innerHTML = `Registered successfully. Logging you in in ${remaining_time} seconds...`;
+        remaining_time--;
+        if (remaining_time == 0) {
+          clearInterval(interval);
+          // window.location.href = '/';
+        }
+      }
+      interval_function();
+      let interval = setInterval(() => {
+        interval_function();
+      }, 1000);
     } else {
-      display_error(json.error);
+      display_message(json.error);
     }
   });
 });
--- public/scripts/trace.js
+++ public/scripts/trace.js
@@ -2,63 +2,62 @@ const body = document.querySelectorAll('body')[0];
 let current_file = null;
 
 // upload dialog window
-const upload_dialog = document.querySelectorAll('#upload-dialog')[0];
-const upload_dialog_window = document.querySelectorAll('#upload-dialog-window')[0];
-const upload_button = document.querySelectorAll('#upload-button')[0];
-const upload_dialog_cancel_button = document.querySelectorAll('#upload-dialog-cancel-button')[0];
-const upload_dialog_upload_button = document.querySelectorAll('#upload-dialog-upload-button')[0];
+const $upload_dialog = document.querySelectorAll('#upload-dialog')[0];
+const $upload_dialog_window = document.querySelectorAll('#upload-dialog-window')[0];
+const $upload_button = document.querySelectorAll('#upload-button')[0];
+const $upload_dialog_cancel_button = document.querySelectorAll('#upload-dialog-cancel-button')[0];
+const $upload_dialog_upload_button = document.querySelectorAll('#upload-dialog-upload-button')[0];
 let upload_dialog_open = false;
 const open_upload_dialog = () => {
-  upload_dialog.classList.remove('hidden');
+  $upload_dialog.classList.remove('hidden');
   upload_dialog_open = true;
 };
 const close_upload_dialog = () => {
-  upload_dialog.classList.add('hidden');
+  $upload_dialog.classList.add('hidden');
   upload_dialog_open = false;
-  upload_dialog_input.value = '';
+  $upload_dialog_input.value = '';
   current_file = null;
-  upload_dialog_window_image.file = null;
-  upload_dialog_window_image.src = '';
+  $upload_dialog_window_image.file = null;
+  $upload_dialog_window_image.src = '';
   file_cleared();
 };
-upload_button.addEventListener('click', () => {
+$upload_button.addEventListener('click', () => {
   open_upload_dialog();
 });
-upload_dialog_cancel_button.addEventListener('click', () => {
+$upload_dialog_cancel_button.addEventListener('click', () => {
   close_upload_dialog();
 });
-upload_dialog_upload_button.addEventListener('click', () => {
+$upload_dialog_upload_button.addEventListener('click', () => {
   if (current_file) {
     upload_file(current_file);
     close_upload_dialog();
   }
 });
-upload_dialog.addEventListener('click', (e) => {
+$upload_dialog.addEventListener('click', (e) => {
   close_upload_dialog();
 });
-upload_dialog_window.addEventListener('click', (e) => {
+$upload_dialog_window.addEventListener('click', (e) => {
   e.stopPropagation();
 });
 
 // upload dialog dropbox
-const upload_dialog_window_image = document.querySelectorAll('#upload-dialog-window-image')[0];
-const upload_dialog_input = document.querySelectorAll('#upload-dialog-input')[0];
-const upload_dialog_placeholder = document.querySelectorAll('#upload-dialog-placeholder')[0];
-const upload_dialog_window_header = document.querySelectorAll('#upload-dialog-window .dialog-header')[0];
-const upload_dialog_placeholder_text = document.querySelectorAll('#upload-dialog-placeholder .placeholder-text')[0];
-let dropbox = document.querySelectorAll('#upload-dialog-window .dialog-top')[0];
+const $upload_dialog_window_image = document.querySelectorAll('#upload-dialog-window-image')[0];
+const $upload_dialog_input = document.querySelectorAll('#upload-dialog-input')[0];
+const $upload_dialog_placeholder = document.querySelectorAll('#upload-dialog-placeholder')[0];
+const $upload_dialog_window_header = document.querySelectorAll('#upload-dialog-window .dialog-header')[0];
+const $upload_dialog_placeholder_text = document.querySelectorAll('#upload-dialog-placeholder .placeholder-text')[0];
 const file_added = () => {
-  upload_dialog_placeholder.classList.add('hidden');
-  upload_dialog_window_image.classList.remove('hidden');
-  upload_dialog_placeholder.classList.remove('green');
-  upload_dialog_window_header.classList.remove('disabled');
-  upload_dialog_upload_button.classList.remove('disabled');
+  $upload_dialog_placeholder.classList.add('hidden');
+  $upload_dialog_window_image.classList.remove('hidden');
+  $upload_dialog_placeholder.classList.remove('green');
+  $upload_dialog_window_header.classList.remove('hidden');
+  $upload_dialog_upload_button.classList.remove('disabled');
 };
 const file_cleared = () => {
-  upload_dialog_placeholder.classList.remove('hidden');
-  upload_dialog_window_image.classList.add('hidden');
-  upload_dialog_window_header.classList.add('disabled');
-  upload_dialog_upload_button.classList.add('disabled');
+  $upload_dialog_placeholder.classList.remove('hidden');
+  $upload_dialog_window_image.classList.add('hidden');
+  $upload_dialog_window_header.classList.add('hidden');
+  $upload_dialog_upload_button.classList.add('disabled');
 };
 const upload_file = (file) => {
   console.log('upload_file called');
@@ -66,33 +65,53 @@ const upload_file = (file) => {
 }
 
 function handleFile(file) {
-  let old_text = upload_dialog_placeholder_text.innerHTML;
-  upload_dialog_placeholder_text.innerHTML = 'Uploading...';
+  if (current_file) {
+    let old_width = $upload_dialog_window_image.offsetWidth;
+    let old_height = $upload_dialog_window_image.offsetHeight;
+    console.log(`old_width: ${old_width}`);
+    console.log(`old_height: ${old_height}`);
+    $upload_dialog_placeholder.style.width = `${old_width}px`;
+    $upload_dialog_placeholder.style.height = `${old_height}px`;
+    $upload_dialog_window_header.classList.add('disabled');
+    $upload_dialog_upload_button.classList.add('disabled');
+    $upload_dialog_window_image.classList.add('hidden');
+    $upload_dialog_placeholder.classList.remove('hidden');
+  }
+  let old_text = $upload_dialog_placeholder_text.innerHTML;
+  $upload_dialog_placeholder_text.innerHTML = 'Uploading...';
   current_file = file;
   console.log(file);
-  upload_dialog_window_image.file = file;
+  $upload_dialog_window_image.file = file;
   const reader = new FileReader();
   reader.onload = (e) => {
-    upload_dialog_window_image.src = e.target.result;
+    $upload_dialog_window_image.src = e.target.result;
+  };
+  reader.onloadend = () => {
+    $upload_dialog_placeholder.style.width = '450px';
+    $upload_dialog_placeholder.style.height = '300px';
+    $upload_dialog_window_header.classList.remove('disabled');
+    $upload_dialog_upload_button.classList.remove('disabled');
+    $upload_dialog_placeholder.classList.add('hidden');
+    $upload_dialog_placeholder_text.innerHTML = old_text;
+    $upload_dialog_window_image.classList.remove('hidden');
     file_added();
-    upload_dialog_placeholder_text.innerHTML = old_text;
   };
   reader.readAsDataURL(file);
 }
 function dragenter(e) {
   e.stopPropagation();
   e.preventDefault();
-  upload_dialog_placeholder.classList.add('green');
+  $upload_dialog_placeholder.classList.add('green');
 }
 function dragover(e) {
   e.stopPropagation();
   e.preventDefault();
-  upload_dialog_placeholder.classList.add('green');
+  $upload_dialog_placeholder.classList.add('green');
 }
 function dragleave(e) {
   e.stopPropagation();
   e.preventDefault();
-  upload_dialog_placeholder.classList.remove('green');
+  $upload_dialog_placeholder.classList.remove('green');
 }
 function drop(e) {
   e.stopPropagation();
@@ -101,18 +120,29 @@ function drop(e) {
   const file = dt.files[0];
   handleFile(file);
 }
-upload_dialog_input.addEventListener('change', (e) => {
+$upload_dialog_input.addEventListener('change', (e) => {
   const file = e.target.files[0];
   handleFile(file);
 });
-dropbox.addEventListener('dragenter', dragenter, false);
-dropbox.addEventListener('dragover', dragover, false);
-dropbox.addEventListener('drop', drop, false);
-dropbox.addEventListener('dragleave', dragleave, false);
+const $dropbox = document.querySelectorAll('#upload-dialog-window .dialog-top')[0];
+$dropbox.addEventListener('dragenter', dragenter, false);
+$dropbox.addEventListener('dragover', dragover, false);
+$dropbox.addEventListener('drop', drop, false);
+$dropbox.addEventListener('dragleave', dragleave, false);
 // keydown events
 document.addEventListener('keydown', (e) => {
   if (e.key == 'Escape') {
     if (upload_dialog_open)
       close_upload_dialog();
   }
+});
+
+/* logout */
+const logout_button = document.querySelectorAll('#logout-button')[0];
+logout_button.addEventListener('click', () => {
+  fetch('/api/logout', {
+    method: 'GET'
+  }).then(() => {
+    window.location.href = '/';
+  });
 });
